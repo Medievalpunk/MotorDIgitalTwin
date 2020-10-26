0de35660277a55609724731d077182d20f6fde15
Sergei Jegorov <oasisbboy.grey@gmail.com>
2020-10-26 00:32:30 -0700

    update thru website

---
+++ parseDewetron.cpp
@@ -0,0 +1,147 @@
+//
+// Created by sejego on 10/18/20.
+// This script parses the data from Dewetron measurement csv file.
+// TODO: umap instead of vector
+// add relative path to file.
+// get onevalue of current?
+//
+
+#include <iostream>
+#include <unordered_map>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include "parseDewetron.h"
+
+// instantiate the object with file name and open it
+parseDewetron::parseDewetron(std::string filename, float frequency)
+{
+    dewetronFile = std::ifstream(filename);
+    usedFrequency = frequency;
+    parseDewetronFile();        //is this ok?
+}
+
+// parse the Dewetron measurements file
+void parseDewetron::parseDewetronFile()
+{
+    bool isFirstLine = true;
+    int arrCounter;
+    std::string element;
+    int buffCounter;
+    if(!dewetronFile.is_open())
+    {
+        throw std::runtime_error("Could not open file");
+    }
+
+    //if file is open and is OK continue
+
+    else if(dewetronFile.good()) {
+        while (std::getline(dewetronFile, line))
+        {
+            if(isFirstLine)                 // check for first line, cuz it contains headers
+            {
+                std::stringstream ss(line); // not sure if needed here
+                isFirstLine = false;
+                continue;
+            }
+                // Create a stringstream from line
+                std::stringstream ss(line);
+                buffCounter = 0;
+                arrCounter = 0;
+
+                // start reading the file line by line
+
+                while (std::getline(ss, element, ','))
+                {
+                    //check for columns that matter
+                    switch(buffCounter)
+                    {
+                        case 0:
+                            bufferArray[arrCounter] = std::stof(element);
+                            arrCounter++;
+                            break;
+                        case 5:
+                            bufferArray[arrCounter] = std::stof(element);
+                            arrCounter++;
+                            break;
+                        case 10:
+                            bufferArray[arrCounter] = std::stof(element);
+                            arrCounter++;
+                            break;
+                        case 15:
+                            bufferArray[arrCounter] = std::stof(element);
+                            arrCounter++;
+                            break;
+                        case 20:
+                            bufferArray[arrCounter] = std::stof(element);
+                            arrCounter++;
+                            break;
+                        case 25:
+                            bufferArray[arrCounter] = std::stof(element);
+                            arrCounter++;
+                            break;
+                        case 30:
+                            bufferArray[arrCounter] = std::stof(element);
+                            break;
+                    }
+                    buffCounter++;
+                }
+                // insert values for appropriate umaps
+                for (int j = 0; j < 7; j++) {
+
+                    //std::cout << bufferArray[j] << "\n";
+
+                    switch (j) {
+                        case 1:
+                            current_1.insert(std::make_pair(bufferArray[0], bufferArray[j]));
+                            break;
+                        case 2:
+                            current_2.insert(std::make_pair(bufferArray[0], bufferArray[j]));
+                            break;
+                        case 3:
+                            current_3.insert(std::make_pair(bufferArray[0], bufferArray[j]));
+                            break;
+                        case 4:
+                            voltage_1.insert(std::make_pair(bufferArray[0], bufferArray[j]));
+                            break;
+                        case 5:
+                            voltage_2.insert(std::make_pair(bufferArray[0], bufferArray[j]));
+                            break;
+                        case 6:
+                            voltage_3.insert(std::make_pair(bufferArray[0], bufferArray[j]));
+                            break;
+                    }
+                }
+            }
+        }
+    dewetronFile.close();
+}
+float parseDewetron::getCurrentOne(float key)
+{
+    return current_1.at(key);
+}
+float parseDewetron::getCurrentTwo(float key)
+{
+    return current_2.at(key);
+}
+float parseDewetron::getCurrentThree(float key)
+{
+    return current_3.at(key);
+}
+float parseDewetron::getVoltageOne(float key)
+{
+    return voltage_1.at(key);
+}
+float parseDewetron::getVoltageTwo(float key)
+{
+    return voltage_2.at(key);
+}
+float parseDewetron::getVoltageThree(float key)
+{
+    return voltage_3.at(key);
+}
+float parseDewetron::getFrequency()
+{
+    return usedFrequency;
+}
